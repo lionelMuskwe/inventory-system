f7d8d0445cca66b9e1b188dc4ab1e7299dc7fe0f
lionelMuskwe <lionel04muskwe@gmail.com>
2023-02-22 02:46:59 +0000

    Application Login functionality working
    If a users has credentials stored in database, the user will be logged in

--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -1,5 +1,9 @@
 package application;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 import javafx.scene.control.Button;
@@ -17,5 +21,34 @@ public class LoginController {
 	private ImageView loginLogo;
 	@FXML
 	private Button loginButton;
-
+	
+	@FXML 
+	public void login(ActionEvent event) {
+		try {
+			System.out.println("Trying to login!!!!!!!!");
+			System.out.println(event.toString());
+			
+			String username = loginUsername.getText();
+			String password = loginPassword.getText();
+			
+			System.out.println(String.format("Details are: %s  and %s ", username, password));
+			establishDatabaseConnection(username, password);
+		}
+		catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	public Connection establishDatabaseConnection(String username, String password) {
+		Connection conn;
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/laravel", username, password);
+			System.out.println("Connected to SQL server successfully");
+			return conn;
+		} catch (Exception ex) {
+			System.out.println("Failed to connect !!!");
+			System.out.println("Error: " + ex.getMessage());
+			return null;
+		}
+	}
 }
