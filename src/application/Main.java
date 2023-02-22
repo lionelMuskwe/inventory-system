acdec4b5295ac49b7c889ad479bc998272d5dcf5
lionelMuskwe <lionel04muskwe@gmail.com>
2023-02-22 02:56:57 +0000

    Added Icon and stage title text

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
@@ -15,6 +16,10 @@ public class Main extends Application {
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getClassLoader().getResource("application/application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			// Setting Stage display details
+			Image icon = new Image("application/assets/images/rr-icon.png");
+			primaryStage.setTitle("Readers Republic Inventory System");
+			primaryStage.getIcons().add(icon);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
