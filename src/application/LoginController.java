b766d7e0dd4e80b9b7b0823f395a6dcba5711aac
lionelMuskwe <lionel04muskwe@gmail.com>
2023-02-22 01:24:33 +0000

    Created login page
    Login functionality not yet implemented

--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -1,5 +1,17 @@
 package application;
 
+import javafx.fxml.FXML;
+
+import javafx.scene.control.TextField;
+
+import javafx.scene.image.ImageView;
+
 public class LoginController {
-	
+	@FXML
+	private TextField loginUsername;
+	@FXML
+	private TextField loginPassword;
+	@FXML
+	private ImageView loginLogo;
+
 }
