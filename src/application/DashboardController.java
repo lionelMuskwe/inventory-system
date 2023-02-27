b8f1957e07be59abf3b9bd569f0dbd9db3e8ec15
lionelMuskwe <lionel04muskwe@gmail.com>
2023-02-27 14:51:17 +0000

    Dashboard allows you to go to search page

---
+++ src/application/DashboardController.java
@@ -0,0 +1,42 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class DashboardController {
+	
+	 @FXML
+    private Button goToInventoryPage;
+
+    @FXML
+    private Button goToSearchPage;
+
+    @FXML
+    private Button gotToStockLevelsPage;
+
+    @FXML
+    private Text loggedInUsername;
+	
+	public void setLoggedInUsername(String username) {
+		this.loggedInUsername.setText(username);
+	}
+	
+	
+	public void openSearchPage(ActionEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("search.fxml"));
+		Parent root = (Parent) loader.load();
+		Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+}
