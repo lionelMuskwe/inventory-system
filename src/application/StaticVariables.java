731688ffcf66d3dcbfac96586a57ded6269597e7
lionelMuskwe <lionel04muskwe@gmail.com>
2023-03-28 15:48:15 +0100

    Renamed singular StaticVariable to plural StaticVariables

--- src/application/StaticVariables.java
+++ src/application/StaticVariables.java
@@ -1,6 +1,6 @@
 package application;
 
-public class StaticVariable {
+public class StaticVariables {
 	public static int ROLE_ADMIN = 2;
 	public static int ROLE_EMPLOYEE = 1;
 	public static int ROLE_USER = 0;
